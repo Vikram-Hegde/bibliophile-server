a73376102fde092419ac0f65070b5d988ce4a79a
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-05-27 20:05:45 +0530

    added compression

--- index.ts
+++ index.ts
@@ -3,10 +3,13 @@ import bookRoutes from "./routes/books";
 import connectToDB from "./database/db";
 import dotenv from "dotenv";
 import cors from "cors";
+import compression from 'compression';
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
+app.use(compression())
 app.use(express.json({ limit: "10mb" }));
 app.use("/books", bookRoutes);
 
